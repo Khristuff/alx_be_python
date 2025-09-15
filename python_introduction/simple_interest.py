93f77aed98ceb5b7f6abb49cf6787e7e529a9f86
Chris <gujo4life@gmail.com>
2025-09-16 00:01:11 +0100

    corrected the error checks

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,8 @@
+
 principal = 1000  # Principal amount in dollars
 rate = 0.05          # Annual interest rate in percent 
 time = 3          # Time in years
 
 # Calculate simple interest
-interest = (principal * rate * time)
+interest = principal * rate * time
 print(f"The simple interest is: {interest:}")
