11e996f1a965f8905590c8588056cfaf451e005b
Chris <gujo4life@gmail.com>
2025-09-22 09:51:42 +0100

    amended the multiplication_table.py file to fix a bug in the print statement.

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 number = input("Enter a number to see its multiplication table: ")
 for i in range(1, 11):
     product = int(number) * i
-    print(number, "x", i = product)
+    print(f"{number} * {i} = {product}")
 
     
